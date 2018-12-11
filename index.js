635f43aa8999212ce61a8fad4af0b199da814f87
sam ghoreishi <sam20gh@gmail.com>
2018-12-11 12:39:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 function dwarfRollCall(dwarves) {
+  var name = []
+  for (var i= 0 ; i <dwarves.length; i++){
+    name += (i + 1) + ". " + dwarves[i] + " ";
+  }
+  return name;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+   var newName = []	;	
+	for (var i = 0; i < planeteerCalls.length; i++) {	
+		var name = planeteerCalls[i].toUpperCase() + "!";	
+		newName.push(name);	
+	}
+	return newName;
 }
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i < words.length; i++) {	
+    if (words[i].length > 4){
+      return true
+    }else
+    {
+      return false
+    }
+  }
+  
 }
 
 function findTheCheese (foods) {
+  var value = ["cheddar" , "gouda" , "camembert"];
+  for (var i = 0; i < foods.length ; i++ ){
+    if ( foods.indexOf(value[i]) > -1){
+      return value[i];
+    }
+    else{
+      return "no cheese!"
+    }
+  }
 }
